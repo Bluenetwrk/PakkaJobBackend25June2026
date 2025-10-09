512dc7678a44b0a4176bc081426d5e894915999d
Blueimpulse2517 <blueimpulse2517@gmail.com>
2025-10-09 17:43:14 +0530

    commit

--- Authcontroller.js
+++ Authcontroller.js
@@ -10,7 +10,7 @@ const getAccessToken = async (code) => {
         code: code,
         client_id: process.env.LINKEDIN_CLIENT_ID,
         client_secret: process.env.LINKEDIN_CLIENT_SECRET,
-        redirect_uri: 'http://localhost:3000/AuthRoute/callback',
+        redirect_uri: 'http://localhost:3000/LinkenIn/callback',
     });
 
     const response = await fetch('https://www.linkedin.com/oauth/v2/accessToken', {
